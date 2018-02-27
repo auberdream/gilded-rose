92cc79edf2ea9866ffe1fab691d151d8c8acee5e
Ellie <elliewem@gmail.com>
2018-02-27 14:30:05 +0000

    Add tests for normal items

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,13 +1,40 @@
-require_relative '../gilded_rose.rb'
+require_relative '../src/gilded_rose.rb'
+require_relative '../src/items.rb'
 
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+    describe "Normal items:" do
+
+      before(:each) do
+        @items = [Item.new("normal item", 4, 10), Item.new("normal item", 0, 5), Item.new("normal item", 2, 0)]
+        GildedRose.new(@items).update_quality()
+      end
+
+      it "decreases the quality by one" do
+        expect(@items[0].quality).to eq 9
+      end
+
+      it "decreases the sell_in by one if sell_in is positive" do
+        expect(@items[0].sell_in).to eq 3
+      end
+
+      it "decreases the sell_in by two if sell_in is negative" do
+        expect(@items[1].quality).to eq 3
+      end
+
+      it "the quality can not be negative" do
+        expect(@items[2].quality).to eq 0
+      end
+
     end
+
+    # describe "Aged Brie:" do
+    #
+    #   it ""
+    #
+    # end
+
   end
 
 end
