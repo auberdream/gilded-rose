65f98018657a4b14fda66d5958e886c913cdbc58
Ellie <elliewem@gmail.com>
2018-02-27 20:27:14 +0000

    Update normal class to accept instance of item

--- ruby/spec/normal_spec.rb
+++ ruby/spec/normal_spec.rb
@@ -1,28 +1,29 @@
 require_relative '../src/normal.rb'
+require_relative '../src/item.rb'
 
 describe Normal do
 
   before(:each) do
-    @normal_item = Normal.new(4, 3)
+    @item = Item.new("normal thing", 4, 3)
   end
 
   it "decreases the quality by one" do
-    expect{ @normal_item.update }.to change{ @normal_item.quality }.from(3).to(2)
+    expect{ subject.update(@item) }.to change{ @item.quality }.from(3).to(2)
   end
 
   it "decreases the sell_in by one if sell_in is positive" do
-    expect{ @normal_item.update }.to change{ @normal_item.sell_in }.from(4).to(3)
+    expect{ subject.update(@item) }.to change{ @item.sell_in }.from(4).to(3)
   end
 
   it "decreases the sell_in by two if sell_in is negative" do
-    normal_item = Normal.new(0, 4)
-    expect{ normal_item.update }.to change{ normal_item.quality }.from(4).to(2)
+    item = Item.new("normal thing", 0, 4)
+    expect{ subject.update(item) }.to change{ item.quality }.from(4).to(2)
   end
 
   it "the quality cannot be negative" do
-    normal_item = Normal.new(3,0)
-    normal_item.update
-    expect(normal_item.quality).to eq 0
+    item = Item.new("normal thing", 4, 0)
+    subject.update(item)
+    expect(item.quality).to eq 0
   end
 
 end
--- ruby/src/normal.rb
+++ ruby/src/normal.rb
@@ -2,10 +2,10 @@ require_relative './initializer.rb'
 
 class Normal < Initializer
 
-  def update
-    decrease_sell_in
-    @quality -= 1 if @quality != 0
-    @quality -= 1 if @sell_in <= 0
+  def update(item)
+    decrease_sell_in(item)
+    item.quality -= 1 if item.quality != 0
+    item.quality -= 1 if item.sell_in <= 0
   end
 
 end
