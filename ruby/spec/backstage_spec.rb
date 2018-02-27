e81e3072c6f81516a5f1d8c366b55c38c5870b07
Ellie <elliewem@gmail.com>
2018-02-27 20:32:24 +0000

    Update backstage class to accept instance of item

--- ruby/spec/backstage_spec.rb
+++ ruby/spec/backstage_spec.rb
@@ -1,25 +1,26 @@
 require_relative '../src/backstage.rb'
+require_relative '../src/item.rb'
 
 describe Backstage do
 
   it "cannot have quality over 50" do
-    backstage = Backstage.new(5, 50)
-    backstage.update
-    expect(backstage.quality).to eq 50
+    item = Item.new("Backstage", 4, 50)
+    subject.update(item)
+    expect(item.quality).to eq 50
   end
 
   describe "more than 10 days sell_in" do
 
     before(:each) do
-      @backstage = Backstage.new(12, 10)
+      @item = Item.new("Backstage", 12, 10)
     end
 
     it "increases quality by one" do
-      expect{ @backstage.update }.to change{ @backstage.quality }.from(10).to(11)
+      expect{ subject.update(@item) }.to change{ @item.quality }.from(10).to(11)
     end
 
     it "decreases the sell_in by one" do
-      expect{ @backstage.update }.to change{ @backstage.sell_in }.from(12).to(11)
+      expect{ subject.update(@item) }.to change{ @item.sell_in }.from(12).to(11)
     end
 
   end
@@ -27,15 +28,15 @@ describe Backstage do
   describe "less than 11 days, more than 5 days sell_in" do
 
     before(:each) do
-      @backstage = Backstage.new(9, 10)
+      @item = Item.new("Backstage", 9, 10)
     end
 
     it "increases quality by two" do
-      expect{ @backstage.update }.to change{ @backstage.quality }.from(10).to(12)
+      expect{ subject.update(@item) }.to change{ @item.quality }.from(10).to(12)
     end
 
     it "decreases the sell_in by one" do
-      expect{ @backstage.update }.to change{ @backstage.sell_in }.from(9).to(8)
+      expect{ subject.update(@item) }.to change{ @item.sell_in }.from(9).to(8)
     end
 
   end
@@ -43,15 +44,15 @@ describe Backstage do
   describe "less than 6 days sell_in" do
 
     before(:each) do
-      @backstage = Backstage.new(5, 10)
+      @item = Item.new("Backstage", 5, 10)
     end
 
     it "increases quality by three" do
-      expect{ @backstage.update }.to change{ @backstage.quality }.from(10).to(13)
+      expect{ subject.update(@item) }.to change{ @item.quality }.from(10).to(13)
     end
 
     it "decreases the sell_in by one" do
-      expect{ @backstage.update }.to change{ @backstage.sell_in }.from(5).to(4)
+      expect{ subject.update(@item) }.to change{ @item.sell_in }.from(5).to(4)
     end
 
   end
@@ -59,11 +60,11 @@ describe Backstage do
   describe "0 days sell_in" do
 
     before(:each) do
-      @backstage = Backstage.new(1, 5)
+      @item = Item.new("Backstage", 1, 5)
     end
 
     it "decreases quality to 0" do
-      expect{ @backstage.update }.to change{ @backstage.quality }.from(5).to(0)
+      expect{ subject.update(@item) }.to change{ @item.quality }.from(5).to(0)
     end
 
   end
--- ruby/src/backstage.rb
+++ ruby/src/backstage.rb
@@ -2,17 +2,17 @@ require_relative './initializer.rb'
 
 class Backstage < Initializer
 
-  def update
-    decrease_sell_in
-    update_quality if @quality < 50
+  def update(item)
+    decrease_sell_in(item)
+    update_quality(item) if item.quality < 50
   end
 
   private
 
-  def update_quality
-    @quality += 1 if @sell_in > 10
-    @quality += 2 if @sell_in > 5 && @sell_in < 11
-    @quality += 3 if @sell_in > 0 && @sell_in < 6
-    @quality = 0 if @sell_in == 0
+  def update_quality(item)
+    item.quality += 1 if item.sell_in > 10
+    item.quality += 2 if item.sell_in > 5 && item.sell_in < 11
+    item.quality += 3 if item.sell_in > 0 && item.sell_in < 6
+    item.quality = 0 if item.sell_in == 0
   end
 end
