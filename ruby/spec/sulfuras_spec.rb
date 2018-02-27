5a5c1412e5e1037efef0150b6d0b7ae760a193ba
Ellie <elliewem@gmail.com>
2018-02-27 20:23:28 +0000

    Update sulfuras class to accept instance of item

--- ruby/spec/sulfuras_spec.rb
+++ ruby/spec/sulfuras_spec.rb
@@ -1,19 +1,20 @@
 require_relative '../src/sulfuras.rb'
+require_relative '../src/item.rb'
 
 describe Sulfuras do
 
   before(:each) do
-    @sulfuras = Sulfuras.new(5,5)
+    @item = Item.new("Sulfuras", 5, 5)
   end
 
   it "does not change the quality" do
-    @sulfuras.update
-    expect(@sulfuras.quality).to eq 5
+    subject.update(@item)
+    expect(@item.quality).to eq 5
   end
 
   it "does not change the sell_in" do
-    @sulfuras.update
-    expect(@sulfuras.sell_in).to eq 5
+    subject.update(@item)
+    expect(@item.sell_in).to eq 5
   end
 
 end
--- ruby/src/sulfuras.rb
+++ ruby/src/sulfuras.rb
@@ -2,7 +2,7 @@ require_relative './initializer.rb'
 
 class Sulfuras < Initializer
 
-  def update
+  def update(item)
   end
 
 end
