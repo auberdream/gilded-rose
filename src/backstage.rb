53efe653682732f3150195f9976c8edd96fbc300
Ellie <elliewem@gmail.com>
2018-02-28 12:06:17 +0000

    Refactor backstage to inherit methods from initializer

--- src/backstage.rb
+++ src/backstage.rb
@@ -10,9 +10,29 @@ class Backstage < Initializer
   private
 
   def update_quality(item)
-    item.quality += 1 if item.sell_in > 10
-    item.quality += 2 if item.sell_in > 5 && item.sell_in < 11
-    item.quality += 3 if item.sell_in > 0 && item.sell_in < 6
-    item.quality = 0 if item.sell_in == 0
+    increase_quality(item) if over_ten_days?(item)
+    increase_quality(item, DOUBLE_INCREASE) if six_to_ten_days?(item)
+    increase_quality(item, TRIPLE_INCREASE) if one_to_five_days?(item)
+    quality_to_zero(item) if zero_days?(item)
+  end
+
+  def over_ten_days?(item)
+    item.sell_in > 10
+  end
+
+  def six_to_ten_days?(item)
+    item.sell_in > 5 && item.sell_in < 11
+  end
+
+  def one_to_five_days?(item)
+    item.sell_in > 0 && item.sell_in < 6
+  end
+
+  def zero_days?(item)
+    item.sell_in == 0
+  end
+
+  def quality_to_zero(item)
+    item.quality = 0
   end
 end
--- src/initializer.rb
+++ src/initializer.rb
@@ -2,17 +2,23 @@ class Initializer
 
   ONE_DAY = 1
   NORMAL_INCREASE = 1
+  DOUBLE_INCREASE = 2
+  TRIPLE_INCREASE = 3
 
   def decrease_sell_in(item)
     item.sell_in -= ONE_DAY
   end
 
-  def increase_quality(item, increase = NORMAL_INCREASE)
-    item.quality += increase
+  def increase_quality(item, amount = NORMAL_INCREASE)
+    item.quality += amount
   end
 
   def quality_under_fifty?(item)
     item.quality < 50
+  end
+
+  def quality_to_zero(item)
+    item.quality = 0
   end
 
 end
