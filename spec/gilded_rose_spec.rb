ebff66df86de0b6c654dda07753e0221a697f067
Ellie <elliewem@gmail.com>
2018-02-28 13:06:04 +0000

    Link Conjured with GildedRose

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,26 +4,32 @@ require_relative '../src/item.rb'
 describe GildedRose do
 
   it "normal items receive the update method" do
-    gilded_rose = GildedRose.new([Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras")
+    gilded_rose = GildedRose.new([Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras", "fakeconjured")
     expect(gilded_rose.normal).to receive(:update)
     gilded_rose.update_quality
   end
 
   it "sulfuras items receive the update method" do
-    gilded_rose = GildedRose.new([Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras")
+    gilded_rose = GildedRose.new([Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras", "fakeconjured")
     expect(gilded_rose.sulfuras).to receive(:update)
     gilded_rose.update_quality
   end
 
   it "aged brie receives the update method" do
-    gilded_rose = GildedRose.new([Item.new(name="Aged Brie", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras")
+    gilded_rose = GildedRose.new([Item.new(name="Aged Brie", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras", "fakeconjured")
     expect(gilded_rose.brie).to receive(:update)
     gilded_rose.update_quality
   end
 
   it "backstage passes receive the update method" do
-    gilded_rose = GildedRose.new([Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras")
+    gilded_rose = GildedRose.new([Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras", "fakeconjured")
     expect(gilded_rose.backstage).to receive(:update)
+    gilded_rose.update_quality
+  end
+
+  it "conjured items receive the update method" do
+    gilded_rose = GildedRose.new([Item.new(name="Conjured Mana Cake", sell_in=10, quality=20)], "fakebackstage", "fakebrie", "fakenormal", "fakesulfuras", "fakeconjured")
+    expect(gilded_rose.conjured).to receive(:update)
     gilded_rose.update_quality
   end
 
--- src/gilded_rose.rb
+++ src/gilded_rose.rb
@@ -3,21 +3,24 @@ require_relative './backstage.rb'
 require_relative './brie.rb'
 require_relative './normal.rb'
 require_relative './sulfuras.rb'
+require_relative './conjured.rb'
 
 class GildedRose
 
-  attr_reader :backstage, :normal, :sulfuras, :brie
+  attr_reader :backstage, :normal, :sulfuras, :brie, :conjured
 
   def initialize(items,
                  backstage = Backstage.new,
                  brie = Brie.new,
                  normal = Normal.new,
-                 sulfuras = Sulfuras.new)
+                 sulfuras = Sulfuras.new,
+                 conjured = Conjured.new)
     @items = items
     @backstage = backstage
     @normal = normal
     @sulfuras = sulfuras
     @brie = brie
+    @conjured = conjured
   end
 
   def update_quality
@@ -31,6 +34,7 @@ class GildedRose
     return @brie if item.name == "Aged Brie"
     return @sulfuras if item.name == "Sulfuras, Hand of Ragnaros"
     return @backstage if item.name == "Backstage passes to a TAFKAL80ETC concert"
+    return @conjured if item.name == 'Conjured Mana Cake'
     @normal
   end
 
